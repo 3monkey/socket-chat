1e95a482d5d88c5eb642fa86b3a221f23c789099
3monkey <csalv.gonz@gmail.com>
2020-05-17 13:09:32 +0200

    Chat finalizado V1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -2,21 +2,27 @@ var socket = io();
 
 var params = new URLSearchParams(window.location.search);
 
-if(!params.has('nombre') || !params.has('sala')){
-	window.location = 'index.html';
-	throw new Error('El nombre y la sala es necesario');
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
-	nombre: params.get('nombre'),
-	sala: params.get('sala')
-}
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
-    socket.emit('entrarChat',usuario, function(res){
-    	console.log('Usuarios Conectados ',res);
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
+        scrollBottom();
     });
+
 });
 
 // escuchar
@@ -28,30 +34,47 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-/*socket.emit('crearMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});*/
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
+    rederizarMensajes(mensaje, false);
+    scrollBottom();
+});
 
-    console.log('Servidor:', mensaje);
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersonas', function(personas) {
+    //console.log(personas);
+    renderizarUsuarios(personas);
+});
+
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+
+    console.log('Mensaje Privado:', mensaje);
 
 });
 
-// Escuchar cambios de usuario
-// Cuando un usuario entra o sale del chat
+function scrollBottom() {
 
-socket.on('listaPersonas', function(mensaje) {
+    // selectors
+    var newMessage = divChatbox.children('li:last-child');
 
-    console.log('Personas:', mensaje);
+    // heights
+    var clientHeight = divChatbox.prop('clientHeight');
+    var scrollTop = divChatbox.prop('scrollTop');
+    var scrollHeight = divChatbox.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight() || 0;
 
-});
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        divChatbox.scrollTop(scrollHeight);
+    }
+}
 
-// mensajes privados
-socket.on('mensajePrivado', function(mensaje){
-	console.log('Mensaje Privado: ', mensaje);
-});
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -17,16 +17,18 @@ io.on('connection', (client) => {
         usuarios.agregarPersona(client.id, data.nombre, data.sala);
         //client.broadcast.emit('listaPersonas', usuarios.getPersonas());
         client.broadcast.to(data.sala).emit('listaPersonas', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('crearMensaje',crearMensaje('Administrador',`${ data.nombre } entró el Chat.`));
         //callback(personas);
         callback(usuarios.getPersonasPorSala(data.sala));
         //console.log(usuario);
     }); 
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
         let persona = usuarios.getPersona(client.id);
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         //client.broadcast.emit('crearMensaje', mensaje);
-        client.broadcast.to(data.sala).emit('crearMensaje', mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+        callback(mensaje);
     });
 
     client.on('disconnect', ()=>{
